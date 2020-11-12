79aa63b40c9f8ce7d05ebf8d96ee5b5894d22883
Justin Rubek <rubejus@bvu.edu>
2020-11-11 21:13:33 -0600

    Updated OpenAPIConverter to use modern constructor

--- flask_restx_patched/model.py
+++ flask_restx_patched/model.py
@@ -1,6 +1,6 @@
 try:
     from apispec.ext.marshmallow.openapi import OpenAPIConverter
-    openapi = OpenAPIConverter(openapi_version='2.0')
+    openapi = OpenAPIConverter(openapi_version='2.0', schema_name_resolver=None, spec=None)
     fields2jsonschema = openapi.fields2jsonschema
     field2property = openapi.field2property
 except ImportError:
